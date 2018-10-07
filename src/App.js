cb3239d4047d76b32d6ce5f23b8a8a2d4c2410d6
Ryan Isler <islerryan@gmail.com>
2018-10-07 11:31:58 -0500

    Added wireframe UI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
+/*
+	TODO:
+
+	- Set height of each grid item.
+
+	- Make the ODD child elements green.
+
+	- Apply (decent) CSS to the children elements
+*/
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<div className="wrapper">
+					<div className="calculator">
+						<div className="calculator-output">
+							0
+						</div>
+
+						<div className="calculator-buttons">
+							<div className="calculator-button C-button">C</div>
+							<div className="calculator-button not-button">+/-</div>
+							<div className="calculator-button percent-button">%</div>
+							<div className="calculator-button divide-button">/</div>
+
+							<div className="calculator-button 7-button">7</div>
+							<div className="calculator-button 8-button">8</div>
+							<div className="calculator-button 9-button">9</div>
+							<div className="calculator-button multiply--button">X</div>
+
+							<div className="calculator-button 4-button">4</div>
+							<div className="calculator-button 5-button">5</div>
+							<div className="calculator-button 6-button">6</div>
+							<div className="calculator-button minus-button">-</div>
+
+							<div className="calculator-button 1-button">1</div>
+							<div className="calculator-button 2-button">2</div>
+							<div className="calculator-button 3-button">3</div>
+							<div className="calculator-button plus-button">+</div>
+
+							<div className="calculator-button 0-button">0</div>
+							<div className="calculator-button -button">0</div>
+							<div className="calculator-button point-button">.</div>
+							<div className="calculator-button equal-button">=</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
